bd2d5fe407b7db5c4f5e88da2fa4dcea3fd4a36b
Groza-Dennis9 <jisatusu.chan99@gmail.com>
2026-04-20 18:59:10 +0300

    Class integration
    Implemented class Ingredients

--- Ingredients.cpp
+++ Ingredients.cpp
@@ -1,5 +1,23 @@
 #include "Ingredients.h"
 
+void Ingredients::validate(int value) const {
+    if (value < 0) throw std::invalid_argument("Stock amount cannot be negative!");
+}
+
+int Ingredients::getBeans() const { return beans; }
+int Ingredients::getMilk() const { return milk; }
+int Ingredients::getWater() const { return water; }
+int Ingredients::getFruit() const { return fruit; }
+int Ingredients::getTea() const { return tea; }
+int Ingredients::getSnacks() const { return snacks; }
+
+void Ingredients::setBeans(int v) { validate(v); beans = v; }
+void Ingredients::setMilk(int v) { validate(v); milk = v; }
+void Ingredients::setWater(int v) { validate(v); water = v; }
+void Ingredients::setFruit(int v) { validate(v); fruit = v; }
+void Ingredients::setTea(int v) { validate(v); tea = v; }
+void Ingredients::setSnacks(int v) { validate(v); snacks = v; }
+
 ostream& operator<<(ostream& os, const Ingredients& i) {
     os << "Beans: " << i.beans << "g | Milk: " << i.milk << "ml | Water: " << i.water << "ml\n"
        << "Fruit: " << i.fruit << "pcs | Tea: " << i.tea << "bags | Snacks: " << i.snacks << "pcs";
--- Ingredients.h
+++ Ingredients.h
@@ -8,9 +8,30 @@ using namespace std;
 class Ingredients {
 private:
     int beans, milk, water, fruit, tea, snacks;
+
+    //Error catching
+    void validate(int value) const;
+
 public:
+    //Constructors
     Ingredients() : beans(0), milk(0), water(0), fruit(0), tea(0), snacks(0) {}
     Ingredients(int b, int m, int w, const int f, int t, int s) : beans(b), milk(m), water(w), fruit(f), tea(t), snacks(s) {}
+
+    //Getters
+    int getBeans() const;
+    int getMilk() const;
+    int getWater() const;
+    int getFruit() const;
+    int getTea() const;
+    int getSnacks() const;
+
+    //Setters
+    void setBeans(int v);
+    void setMilk(int v);
+    void setWater(int v);
+    void setFruit(int v);
+    void setTea(int v);
+    void setSnacks(int v);
 
     friend ostream& operator<<(ostream& os, const Ingredients& i);
 };
